2c6189bddd8cdf4de7ba40f874c213735e4d5b22
AashayShingre <akshingre@gmail.com>
2023-08-20 20:14:53 +0530

    chrome-plugin initial

--- backend/main.py
+++ backend/main.py
@@ -3,32 +3,64 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 
-from fastapi.middleware.cors import CORSMiddleware  
-from pydantic import BaseModel  
-  
-app = FastAPI()  
-  
-class Item(BaseModel):  
-    username: str  
-    amount: float  
-    #url:str
-  
-origins = [  
-    "https://8000-shree970-skeptick-201scrltfd6.ws-us104.gitpod.io"  
-]  
-  
-app.add_middleware(  
-    CORSMiddleware,  
-    allow_origins=["*"],  # Allows all origins  
-    allow_credentials=True,  
-    allow_methods=["*"],  # Allows all methods  
-    allow_headers=["*"],  
-)  
-  
-@app.post("/invest")  
-async def create_item(item: Item):  
-    print(f"Username: {item.username}, Amount: {item.amount}")  
-    return {"Username": item.username, "Amount": item.amount} 
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# class Item(BaseModel):
+#     username: str
+#     amount: float
+#     # url:str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],
+)
+
+
+@app.get("/whole-truth")
+async def get_whole_truth():
+    return {"message": "the whole truth"}
+
+
+class RegisterRequest(BaseModel):
+    username: str
+    amount: str
+    age: str
+
+
+@app.post("/register")
+async def register(registerRequest: RegisterRequest):
+    print(registerRequest)
+    return registerRequest
+
+
+# @app.post("/invest")
+# async def create_item(item: Item):
+#     print(f"Username: {item.username}, Amount: {item.amount}")
+#     return {"Username": item.username, "Amount": item.amount}
+
+
+# @app.post('/register'):
+# amount,
+# usernmae
+
+# # add to user.json
+# #returns 200 ok
+
+
+# @app.post('/transcribe'):
+# youtube_url,
+
+# # returns {}
+
+# @app.get('wholetruth')
+# @app.get('wholet')
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
@@ -37,8 +69,8 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     content = {"status_code": 10422, "message": exc_str, "data": None}
     return JSONResponse(
         content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )  
+    )
 
 if __name__ == "__main__":
-   import uvicorn
-   uvicorn.run(app, host="0.0.0.0")
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0")
--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,9 +1,24 @@
-document.getElementById('investForm').onsubmit = function(e) {  
-    e.preventDefault();  
-  
-    const username = document.getElementById('username').value;  
-    const amount = document.getElementById('amount').value;  
-  
-    // Send a message to the background script  
-    chrome.runtime.sendMessage({username: username, amount: amount});  
-};
+// document.getElementById('investForm').onsubmit = function(e) {
+//     e.preventDefault();
+
+//     const username = document.getElementById('username').value;
+//     const amount = document.getElementById('amount').value;
+
+//     // Send a message to the background script
+//     chrome.runtime.sendMessage({username: username, amount: amount});
+// };
+
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get(["registered", "username"], function (result) {
+    if (result.registered) {
+      document.getElementById("not-registered").style.display = "none";
+      console.log("Todo - fetch the transcript if its there");
+    } else {
+      document.getElementById("registered").style.display = "none";
+      const registerButton = document.getElementById("register-button");
+      registerButton.addEventListener("click", function () {
+        chrome.tabs.create({ url: "register.html" });
+      });
+    }
+  });
+});
