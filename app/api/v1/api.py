bc823bd94b2ec21da66fe94e58e2603858a88d9c
shree970 <shreeyashsanjay.pawar@philips.com>
2023-08-20 22:18:28 +0530

    added wholetruth module

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.api.v1.endpoints import transcribe
 from app.api.v1.endpoints import stock_tips
+from app.api.v1.endpoints import wholetruth
 
 v1_router = APIRouter()
 
 v1_router.include_router(transcribe.router, prefix="/v1")
 v1_router.include_router(stock_tips.router, prefix="/v1")
+v1_router.include_router(wholetruth.router, prefix="/v1")
